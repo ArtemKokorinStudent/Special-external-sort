2dc11cf10b814410c9cab9a6f70c432c6e563948
TrigDeveloper1 <xx.artem-kokorin@mail.ru>
2016-11-15 09:44:44 +0300

    Update shared_pointer.hpp

--- include/shared_pointer.hpp
+++ include/shared_pointer.hpp
@@ -4,7 +4,7 @@ template <typename T>
 class shared_pointer {
 public:
 	shared_pointer(); /*noexcept*/
-	shared_pointer(T * pointer); /*strong*/
+	shared_pointer(T * object_address); /*strong*/
 	shared_pointer(shared_pointer const & other_shared_pointer); /*noexcept*/
 	auto operator=(shared_pointer const & other_shared_pointer) -> shared_pointer &; /*noexcept*/
 	shared_pointer(shared_pointer && other_shared_pointer); /*noexcept*/
@@ -22,6 +22,12 @@ private:
 	size_t * counter;
 };
 
+template <typename T, class ...Args>
+auto make_shared(Args && ...args) -> shared_pointer<T>
+{
+	return shared_pointer<T>(new T(std::forward<Args>(args)...));
+}
+
 template <typename T>
 shared_pointer<T>::shared_pointer() : 
 	pointer(nullptr), 
@@ -31,9 +37,9 @@ shared_pointer<T>::shared_pointer() :
 }
 
 template <typename T>
-shared_pointer<T>::shared_pointer(T* pointer) : 
-	pointer(pointer),
-	counter(new size_t(1)) 
+shared_pointer<T>::shared_pointer(T * object_address) :
+	pointer(object_address),
+	counter(new size_t(1))
 {
 	;
 }
@@ -71,7 +77,7 @@ template <typename T>
 auto shared_pointer<T>::operator=(shared_pointer && other_shared_pointer) -> shared_pointer&
 {
 	if (this != &other_shared_pointer) {
-		this->swap(other_shared_pointer);
+		(shared_pointer<T>(std::move(other_shared_pointer))).swap(*this);
 	}
 	return *this;
 }
@@ -98,13 +104,15 @@ void shared_pointer<T>::swap(shared_pointer & other_shared_pointer)
 template <typename T>
 void shared_pointer<T>::reset()
 {
-	(*counter)--;
-	if ((*counter) == 0) {
-		delete pointer;
-		delete counter;
+	if (counter != nullptr) {
+		(*counter)--;
+		if ((*counter) == 0) {
+			delete pointer;
+			delete counter;
+		}
+		counter = nullptr;
 	}
 	pointer = nullptr;
-	counter = nullptr;
 }
 
 template <typename T>
