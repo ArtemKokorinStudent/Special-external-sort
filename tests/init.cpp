43ed0dfa8eb376646ecda4b7bf068e8a5b530643
TrigDeveloper1 <xx.artem-kokorin@mail.ru>
2016-11-20 23:08:15 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -50,14 +50,14 @@ bool isANotMoreThanB(person A, person B) {
 }
 
 SCENARIO("Sort", "[s]") {
-	size_t n_persons = 10000000;
-	size_t RAM_amount = 40 * 1000 * 1000;
-	std::string names_file_name = "F:\\1\\names.txt";
-	std::string surnames_file_name = "F:\\1\\surnames.txt";
-	std::string database_file_name = "F:\\1\\database.txt";
-	std::string output_file_name = "F:\\1\\sorted_database.txt";
+	size_t n_persons = 2000;
+	size_t RAM_amount = 10000;
+	std::string names_file_name = "names.txt";
+	std::string surnames_file_name = "surnames.txt";
+	std::string database_file_name = "database.txt";
+	std::string output_file_name = "sorted_database.txt";
 	{
-		//Database_Creator database_creator(database_file_name, names_file_name, surnames_file_name, n_persons);
+		Database_Creator database_creator(database_file_name, names_file_name, surnames_file_name, n_persons);
 		Database_Sorter database_sorter(database_file_name, output_file_name, RAM_amount);
 		size_t start = clock();
 		database_sorter.sortDatabase();
@@ -65,7 +65,7 @@ SCENARIO("Sort", "[s]") {
 		std::cout << result << std::endl;
 		system("pause");
 	}
-	for (size_t i = 0; i < 0; i++) {
+	for (size_t i = 0; i < 20; i++) {
 		size_t person1_i = rand() % n_persons;
 		size_t person2_i = person1_i + rand() % (n_persons - person1_i);
 		person person1 = readPerson(output_file_name, person1_i);
