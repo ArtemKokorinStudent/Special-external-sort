3f5cc90fb9647aa88758d3b2ca5f4e1f1f47e85a
TrigDeveloper1 <xx.artem-kokorin@mail.ru>
2016-11-28 06:43:23 +0300

    Update Database_Sorter.hpp

--- include/Database_Sorter.hpp
+++ include/Database_Sorter.hpp
@@ -5,22 +5,6 @@
 #include <limits>
 #include "person.hpp"
 
-/*char russian_letters[6];
-size_t russianLetterI(const char letter, const bool is_capital) {
-	int first_letter_code = static_cast<int>(is_capital ? russian_letters[0] : russian_letters[1]);
-	if (letter > (is_capital ? russian_letters[2] : russian_letters[3])) {
-		size_t result = static_cast<int>(letter) - first_letter_code + 2;
-		return result;
-	}
-	else {
-		if (letter == (is_capital ? russian_letters[4] : russian_letters[5])) {
-			return 7;
-		}
-		size_t result = static_cast<int>(letter) - first_letter_code + 2;
-		return result;
-	}
-}*/
-
 using Buckets = std::vector<std::vector<person>>;
 using Persons = std::vector<person>;
 class Persons_File {
