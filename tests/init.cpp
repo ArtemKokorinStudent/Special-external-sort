3dfa1cc5255de1731ae19170ad5286be378f6378
TrigDeveloper1 <xx.artem-kokorin@mail.ru>
2016-12-02 17:45:37 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -5,15 +5,17 @@
 SCENARIO("Sort", "[s]") {
 	const std::string output_file_name = "F:\\1\\sorted_database.txt";
 	size_t start = clock();
-	externalSort("32.txt", "F:\\1\\sorted_database.txt", 1, "F:\\1\\temp_files\\");
-	std::cout << clock() - start << std::endl;
+	externalSort("8.txt", "F:\\1\\sorted_database.txt", 1);
+	externalSort("16.txt", "F:\\1\\sorted_database.txt", 1);
+	externalSort("32.txt", "F:\\1\\sorted_database.txt", 1);
+	std::cout << "Sort time: " << clock() - start << std::endl;
 
-	const int n_persons = 100;
-	/*for (size_t i = 0; i < 0; i++) {
+	const int n_persons = 300;
+	for (size_t i = 0; i < 100; i++) {
 		size_t person1_i = rand() % n_persons;
 		size_t person2_i = person1_i + rand() % (n_persons - person1_i);
 		person person1 = readPersonFromFile(output_file_name, person1_i);
 		person person2 = readPersonFromFile(output_file_name, person2_i);
 		REQUIRE(person1.name <= person2.name);
-	}*/
+	}
 }
