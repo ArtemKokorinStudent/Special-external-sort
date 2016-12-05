617f02e7e3d7c28594289abc246925163ab93e93
TrigDeveloper1 <xx.artem-kokorin@mail.ru>
2016-12-05 10:14:24 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -29,7 +29,7 @@ SCENARIO("Sort", "[s]") {
 	start = clock();
 	externalSort("16.txt", "F:\\1\\sorted_database.txt", 1);
 	std::cout << "Sort time 15(microseconds): " << clock() - start << std::endl;
-	std::cout << "File was: " << fileSize("16.txt") << std::endl;
+	std::cout << "File was: " << fileStr(output_file_name) << fileSize("16.txt") << std::endl;
 	std::cout << "Output size: " << fileStr("16.txt") << " " << fileSize(output_file_name)<< std::endl;
 	start = clock();
 	externalSort("32.txt", "F:\\1\\sorted_database.txt", 1);
